d8749455754349fcc432f55ee8867af77bda82dc
t.ruspekhofer <t.ruspekhofer@gmail.com>
2020-08-15 22:28:54 +0200

    Removed old user structures needed for savefile conversion

--- Raidbot/Users/UserManagement.cs
+++ Raidbot/Users/UserManagement.cs
@@ -1,8 +1,6 @@
 ﻿using Discord;
-using Discord.WebSocket;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Raidbot.Users
@@ -80,79 +78,6 @@ namespace Raidbot.Users
                 {
                     await user.ModifyAsync(p => p.Nickname = nickname);
                 }
-            }
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //discordId, User
-        private static Dictionary<ulong, UserOld> _oldUsers;
-
-        private static void RewriteSaveFile()
-        {
-            if (File.Exists(_jsonFile))
-            {
-                string json = File.ReadAllText(_jsonFile);
-                _oldUsers = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<ulong, UserOld>>(json);
-                Dictionary<ulong, User> newUsers = new Dictionary<ulong, User>();
-                foreach (var oldUser in _oldUsers)
-                {
-                    User newUser = new User(oldUser.Value.Guilds.First());
-                    newUser.Name = oldUser.Value.Name;
-                    foreach (var account in oldUser.Value.GuildWars2Accounts)
-                    {
-                        if (string.IsNullOrEmpty(account.Value))
-                        {
-                            newUser.AddAccount("GuildWars2", account.Value, out _);
-                        }
-                        else
-                        {
-                            newUser.AddAccount("GuildWars2", account.Key, out string _);
-                        }
-                    }
-                    newUser.SetMainAccount(oldUser.Value.MainAccount);
-                    newUsers.Add(oldUser.Key, newUser);
-                }
-
-                string newJson = Newtonsoft.Json.JsonConvert.SerializeObject(newUsers);
-                File.WriteAllText(_jsonFile, newJson);
-            }
-        }
-
-        private class UserOld
-        {
-            //AccountName, ApiKey
-            public Dictionary<string, string> GuildWars2Accounts { get; set; }
-
-            public string MainAccount { get; set; }
-
-            public string Name { get; set; } = string.Empty;
-
-            public List<ulong> Guilds { get; set; }
-
-            public UserOld()
-            {
-                GuildWars2Accounts = new Dictionary<string, string>();
-                Guilds = new List<ulong>();
-            }
-
-            public UserOld(string accountName, string apiKey)
-            {
-                GuildWars2Accounts = new Dictionary<string, string>();
-                GuildWars2Accounts.Add(accountName, apiKey);
-                Guilds = new List<ulong>();
-                MainAccount = accountName;
             }
         }
     }
